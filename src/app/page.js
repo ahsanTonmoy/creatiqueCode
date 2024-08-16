e342453df5ad2f0464ac4cdfcf5a7fc993faa5c6
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-17 01:26:33 +0600

    add home page bannar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import Bannar from '@/components/pages/home/bannar/Bannar';
 import React from 'react';
 
 const page = () => {
   return (
     <div>
       <div className='container h-[3000px]'>
-        upcomeing section
+        <Bannar/>
       </div>
     </div>
   );
---
+++ src/components/pages/home/bannar/Bannar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Bannar = () => {
+    return (
+        <div>
+            <div className='homeBannar' >
+                <div className='inner-content p-4 lg:p-20 grid gap-4'>
+                    <div className=' text-7xl font-semibold text-center leading-normal'>
+                        Increase your web traffic with our expert
+                    </div>
+
+                    <div className='text-center text-3xl px-8 text-gray-400'>
+                        Agency work with top rated talented people provide qulaity services.
+                    </div>
+
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Bannar;
