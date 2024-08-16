ebdd92932a85e6b91a883542082a1e484b48720d
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-17 00:31:15 +0600

    change menu position & remove shadow

--- src/components/uiComponent/header/Header.jsx
+++ src/components/uiComponent/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     const [open, setOpen] = useState(false)
     return (
         <div>
-            <header className="shadow-lg py-6 px-8">
+            <header className=" py-6 px-8">
                 <div className="inner-content flex justify-between gap-4">
                     {/* brand */}
                     <div className="brand w-32">
@@ -19,29 +19,32 @@ const Header = () => {
                     {/* for large device */}
                     <div className="  gap-16 md:flex hidden">
                         <Menu style={' flex gap-6'} />
-
-                        <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
                     </div>
 
                     {/* for large device */}
-                    <div className=" text-[#095859] text-2xl py-2 md:hidden cursor-pointer">
-                        <div className="" onClick={() => setOpen(!open)}>
-                            <AiOutlineBars />
+                    <div>
+                        <div className="hidden lg:block">
+                            <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
                         </div>
-                        {/*  */}
-                        {
-                            open ? <div className="sidebar fixed top-0 left-0 w-full h-screen text-base">
-                                <div className="menu bg-[#fff] w-60 h-screen p-4">
-                                    <div className="text-[#095859] text-2xl grid justify-items-end gap-6" onClick={() => setOpen(!open)}>
-                                        <IoClose />
-                                    </div>
-                                    <div className="grid gap-20 my-4">
-                                        <Menu style={'grid gap-2 text-black'} />
+                        <div className=" text-[#095859] text-2xl py-2 md:hidden cursor-pointer">
+                            <div className="" onClick={() => setOpen(!open)}>
+                                <AiOutlineBars />
+                            </div>
+                            {/*  */}
+                            {
+                                open ? <div className="sidebar fixed top-0 left-0 w-full h-screen text-base">
+                                    <div className="menu bg-[#fff] w-60 h-screen p-4">
+                                        <div className="text-[#095859] text-2xl grid justify-items-end gap-6" onClick={() => setOpen(!open)}>
+                                            <IoClose />
+                                        </div>
+                                        <div className="grid gap-20 my-4">
+                                            <Menu style={'grid gap-2 text-black'} />
 
-                                        <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
+                                            <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
+                                        </div>
                                     </div>
-                                </div>
-                            </div> : null}
+                                </div> : null}
+                        </div>
                     </div>
                 </div>
             </header>
