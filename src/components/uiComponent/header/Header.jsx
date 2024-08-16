71cbc368744cb7d91676980deb8a3cb08997ed50
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-16 23:52:41 +0600

    add side bar in project

--- src/components/uiComponent/header/Header.jsx
+++ src/components/uiComponent/header/Header.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import brand from '@/assects/image/brand/logo.png'
 import Image from 'next/image';
-import { menuItems } from '@/utills/menu/menuItems';
 import Menu from './../menuUI/Menu';
 import FillBtn from '../buttons/FillBtn';
+import { AiOutlineBars } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
 const Header = () => {
+    const [open, setOpen] = useState(false)
     return (
         <div>
             <header className="shadow-lg py-6 px-8">
@@ -13,13 +16,33 @@ const Header = () => {
                     <div className="brand w-32">
                         <Image src={brand} alt='logo-creatiqucode' />
                     </div>
-
-                    <div className=" flex gap-16">
+                    {/* for large device */}
+                    <div className="  gap-16 md:flex hidden">
                         <Menu style={' flex gap-6'} />
 
-                        <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'}/>
+                        <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
                     </div>
 
+                    {/* for large device */}
+                    <div className=" text-[#095859] text-2xl py-2 md:hidden cursor-pointer">
+                        <div className="" onClick={() => setOpen(!open)}>
+                            <AiOutlineBars />
+                        </div>
+                        {/*  */}
+                        {
+                            open ? <div className="sidebar fixed top-0 left-0 w-full h-screen text-base">
+                                <div className="menu bg-[#fff] w-60 h-screen p-4">
+                                    <div className="text-[#095859] text-2xl grid justify-items-end gap-6" onClick={() => setOpen(!open)}>
+                                        <IoClose />
+                                    </div>
+                                    <div className="grid gap-20 my-4">
+                                        <Menu style={'grid gap-2'} />
+
+                                        <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
+                                    </div>
+                                </div>
+                            </div> : null}
+                    </div>
                 </div>
             </header>
         </div>
