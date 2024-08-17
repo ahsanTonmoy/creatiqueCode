7f1c29b142e59c8bd9f0cb85d95f5777ed7d02ce
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-17 13:15:02 +0600

    footer ui implement

---
+++ src/components/shared/footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className='grid grid-cols-1 md:grid-cols-5'>]
+                {/* footerItem - logo */}
+                <div className='footerItem'>logo</div>
+                {/* footerItem - logo */}
+                <div className='footerItem'>menu</div>
+                {/* footerItem - logo */}
+                <div className='footerItem'>servioces</div>
+                {/* footerItem - logo */}
+                <div className='footerItem'>products</div>
+                {/* footerItem - logo */}
+                <div className='footerItem'>contact</div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
