5c1c72a2c2f7ed396f8576923306604884e836d5
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-17 13:05:40 +0600

    remove page height

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      <div className='container h-[3000px]'>
+      <div className='container'>
         <Bannar/>
       </div>
     </div>
