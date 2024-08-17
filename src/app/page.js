7d965554e1fbd6e6db6ab4747109421be90e72a5
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-17 14:27:15 +0600

    add secendary bg

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,13 @@ import React from 'react';
 const page = () => {
   return (
     <div>
+      {/* f */}
       <div className='container'>
         <Bannar/>
+      </div>
+      {/* s */}
+      <div className='secendary-bg container'>
+        chack
       </div>
     </div>
   );
