4c4fe1fda1e227625247702df714460928f9f586
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-17 23:36:07 +0600

    add consletion section ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Bannar from '@/components/pages/home/bannar/Bannar';
+import Consuletion from '@/components/pages/home/consuletion/Consuletion';
 import Services from '@/components/pages/home/services/Services';
 import React from 'react';
 
@@ -12,6 +13,9 @@ const page = () => {
       {/* s */}
       <div className='secendary-bg container'>
         <Services/>
+      </div>
+      <div className='container'>
+        <Consuletion/>
       </div>
     </div>
   );
---
+++ src/components/pages/home/consuletion/Consuletion.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+const Consuletion = () => {
+    return (
+        <div>
+            <div className='md:py-20 py-4'>
+                <div className='inner-content'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
+                        {/* title */}
+                        <div className='grid gap-8'>
+                            {/* title */}
+                            <div className='title md:text-6xl text-3xl leading-tight'>
+                                Let’s talk <br></br>
+                                with expereince advisors.
+                            </div>
+
+                            {/* sub title */}
+                            <div className=' text-lg md:text-3xl'>
+                                eiusmod tempor incididunt. Ut enim mim veniam, quis nostrud elit.
+                            </div>
+
+                            {/*  */}
+                            <div>
+                                <div className='text-[#095859] text-2xl capitalize my-4'>urgent?</div>
+
+                                <div className=' flex text-xl gap-2 capitalize '>call us <div className='-my-2 text-3xl '> +88 019XXXXXX</div></div>
+                            </div>
+                        </div>
+
+                        {/* from */}
+                        <div className=' p-4 md:p-8 secendary-bg rounded-lg'>
+                            <form action='#'>
+                                <div className='grid gap-4'>
+                                    {/* name */}
+                                    <div className='form-control'>
+                                        <input type='text' name='name' placeholder='your name *' className='bg-white p-4 w-full capitalize rounded-lg border-4'/>
+                                    </div>
+                                    {/* name */}
+                                    <div className='form-control'>
+                                        <input type='email' name='email' placeholder='email *' className='bg-white p-4 w-full capitalize rounded-lg border-4'/>
+                                    </div>
+                                    {/* name */}
+                                    <div className='form-control'>
+                                        <textarea type='text' name='msg' placeholder='your messeges' className='bg-white p-4 w-full capitalize rounded-lg border-4'></textarea>
+                                    </div>
+                                    {/* name */}
+                                    <div className='form-control'>
+                                        <input type='submit' value={'SEND MESSAGE'} className='fillBtn w-full text-white p-4 rounded-lg cursor-pointer font-bold'></input>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Consuletion;
