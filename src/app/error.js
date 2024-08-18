ef8066478b257a573676ae782b0c8dccfa67687c
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-18 13:23:54 +0600

    add error page

---
+++ src/app/error.js
@@ -0,0 +1,21 @@
+'use client'
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <div className='container py-4 md:py-20'>
+                <div className='inner-content grid place-content-center text-center'>
+                    <div className=''>
+                        {/*  */}
+                        <div className='text-4xl'>
+                            Opps! you’r on the wrong place.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Error;
--- src/utills/menu/menuItems.js
+++ src/utills/menu/menuItems.js
@@ -7,38 +7,18 @@ export const menuItems = [
         route: 'about',
         path: '/about',
     },
-    {
-        route: 'product',
-        path: '/product',
-    },
+    
     {
         route: 'services',
         path: '/services',
-        children: [
-            {
-                route: 'web design',
-                path: '/web-design',
-            },
-            {
-                route: 'web design',
-                path: '/web-design',
-            },
-            {
-                route: 'web design',
-                path: '/web-design',
-            },
-            {
-                route: 'web design',
-                path: '/web-design',
-            },
-        ]
+    },
+    {
+        route: 'artical',
+        path: '/artical',
     },
     {
         route: 'contact',
         path: '/contact',
     },
-    {
-        route: 'portfolio',
-        path: '/portfolio',
-    },
+    
 ]
