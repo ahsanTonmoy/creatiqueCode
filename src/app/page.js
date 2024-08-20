b11ff8223cdc107179fc73d50644b60f4730f848
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-20 19:23:57 +0600

    add section bg

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Consuletion from '@/components/pages/home/consuletion/Consuletion';
 import News from '@/components/pages/home/news/News';
 import Services from '@/components/pages/home/services/Services';
 import Testimonal from '@/components/pages/home/testimonal/Testimonal';
+import SectionBG from '@/components/uiComponent/sectionBg/SectionBG';
 import React from 'react';
 
 const page = () => {
@@ -11,27 +12,28 @@ const page = () => {
     <>
       {/* f */}
       <div className='px-4 md:px-20'>
-        <Bannar/>
+        <Bannar />
       </div>
-      {/* s */}
-      <div className='secendary-bg px-4 md:px-20'>
-        <Services/>
-      </div>
-      {/* f */}
-      <div className='secendary-bg px-4 md:px-20'>
-        <Consuletion/>
-      </div>
+      <SectionBG
+        bgColor={'secendary-bg'}
+        content={<div>
+          <Services />
+          <Consuletion />
+        </div>
+        }      >
+      </SectionBG>
+
       {/* s */}
       <div className=' px-4 md:px-20'>
-        <Testimonal/>
+        <Testimonal />
       </div>
       {/* f */}
       <div className='secendary-bg px-4 md:px-20'>
-        <Articals/>
+        <Articals />
       </div>
       {/* s */}
       <div className='secendary-bg px-4 md:px-20'>
-        <News/>
+        <News />
       </div>
     </>
   );
---
+++ src/components/uiComponent/sectionBg/SectionBG.jsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import shapeL from '@/assects/image/shape/shape_88.svg'
+import shapeR from '@/assects/image/shape/shape_89.svg'
+
+
+const SectionBG = ({ bgColor, xFector, yFector, content}) => {
+    return (
+        <div>
+            {/*  */}
+            <div className={`${bgColor} relative`}>
+                <div className="p3 md:p-20">
+                    {/*  */}
+                    <div className={`${yFector}`}>
+                        <div className="shapes shape-one rounded-[50%] absolute  w-9 h-9 right-[21%] -top-5 bg-[#007BFF] "></div>
+                        <div class="shapes shape-two rounded-[50%] absolute  w-9 h-9 left-[30%] -bottom-5 bg-[#FF7228]"></div>
+                        <div class="shapes shape-three rounded-[50%] absolute  w-2.5 h-2.5 animate-[jumpThree_5s_infinite_linear] left-[4%] top-[43%] bg-[#00D8AF]"></div>
+                    </div>
+                    {/*  */}
+                    <div className={`${xFector}`}>
+                    <Image src={shapeL} alt="" className="lazy-img shapes shape-four absolute left-0 bottom-[6%]"/>
+                    <Image src={shapeR} alt="" className="lazy-img shapes shape-five absolute right-0 top-[6%]" style=""></Image>
+                    </div>
+                    <div>
+                        {content}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SectionBG;
