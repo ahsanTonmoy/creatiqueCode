a4513e2e6cef62f1121a9699b4a64c8871edb5c5
Ahsan Tonmoy <ahosant82@gmail.com>
2024-08-22 18:11:05 +0600

    bugs fixed

--- src/components/uiComponent/menuUI/Menu.jsx
+++ src/components/uiComponent/menuUI/Menu.jsx
@@ -11,7 +11,8 @@ const Menu = ({style}) => {
             <ul className={`${style}`}>
                 {
                     menuItems.map(menu =>
-                        <li className='menuItem relative capitalize font-bold text-lg' tabIndex={0} role="button" key={menu}><Link className={`${pathName === menu.path ? ' border-l-2 lg:border-l-0 lg:border-b pb-1 px-2 border-[#095859] hover:text-[#095859] transition-all duration-100': 'px-2 lg:hover:text-[#095859]'}`} href={menu.path}>{menu.route}</Link>
+                        <li className='menuItem relative capitalize font-bold text-lg' key={menu.route}>
+                            <Link className={`${pathName === menu.path ? ' border-l-2 lg:border-l-0 lg:border-b pb-1 px-2 border-[#095859] hover:text-[#095859] transition-all duration-100': 'px-2 lg:hover:text-[#095859]'}`} href={menu.path}>{menu.route}</Link>
                         </li>
                     )
                 }
