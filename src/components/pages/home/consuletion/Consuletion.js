b12117d8ba9886d11b050ca0cfe433e282e7f71e
Ahsanur Rahman Tonmoy <116573300+ahsanTonmoy@users.noreply.github.com>
2024-08-27 14:09:03 +0000

    add

--- src/components/pages/home/consuletion/Consuletion.js
+++ src/components/pages/home/consuletion/Consuletion.js
@@ -40,13 +40,13 @@ const Consuletion = () => {
                             </div>
                         </div>
                         <div className=' p-4 md:p-8 bg-white rounded-lg relative '>
-                            <div className=' absolute -top-10 -left-6 -rotate-5 hidden md:block text-center '>
+                            <div className=' absolute -top-10 -left-6 -rotate-6 hidden md:block text-center '>
                                 <div>
                                     Fill the <br></br> form 
                                     </div>
 
-                                <div className='grid justify-items-center '>
-                                    <Image className='grid justify-items-center  ' src={shape90} alt='' />
+                                <div className='- px-44 - '>
+                                    <Image className='   ' src={shape90} alt='' />
                                  </div>       
 
                          </div>
