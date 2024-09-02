b951152f85fbed2b0d009324be5765556dc72aa9
Ahsan Tonmoy <ahosant82@gmail.com>
2024-09-03 03:00:31 +0600

    change ui

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import loader from '../assects/image/preloader-crop.gif'
 import Image from 'next/image';
-const Loading = () => {
+const loading = () => {
     return (
         <div>
-            <div cLassName='grid place-content-center bg-white h-screen absolute top-0 left-0 z-[9999] w-screen'>
+            <div className='grid place-content-center bg-white h-screen absolute top-0 left-0 z-[9999] w-screen'>
                 <Image src={loader} alt='' />
             </div>
         </div>
     );
 };
 
-export default Loading;
+export default loading;
