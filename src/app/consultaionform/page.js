bcbe104b744003adbcf3a754d3d45910105fc607
Ahsan Tonmoy <ahosant82@gmail.com>
2024-10-12 16:50:04 +0600

    consultation form implement

---
+++ src/app/consultaionform/page.js
@@ -0,0 +1,70 @@
+'use client'
+import React from 'react';
+import Preloader from '@/components/shared/preloader/Preloader';
+import PageBannar from '@/components/uiComponent/pageBannar/PageBannar';
+const page = () => {
+    const hendleform = e => {
+        e.preventDefault()
+
+        const msg = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            msg: e.target.msg.value,
+            attachFile: e.target.attachFile.value,
+        }
+
+        console.log(msg);
+    }
+    return (
+        <div>
+            <Preloader />
+            <div>
+                <PageBannar titile={'consultation form'}></PageBannar>
+                {/*  */}
+                <div className='md:px-4'>
+                    <div className='pageBannar'>
+                    {/*  */}
+                        <div className="md:p-20 p-4 pageContainer">
+                        {/* form */}
+							<div className="contact-section-two text-left p-12 bg-white shadow-2xl">
+								<div className=" md:mb-[60px] sm:mb-[60px] xsm:mb-[60px] wow fadeInLeft" >
+									<form onSubmit={hendleform}>
+										<div className='grid gap-4 capitalize'>
+											{/* name */}
+											<div className='form-control'>
+												<label className="block text-[14px] text-[rgba(0,0,0,0.5)] mb-[7px]">Name*</label>
+												<input type='text' name='name' placeholder='your name *' className='bg-white p-4 w-full capitalize rounded-lg border-4' required />
+											</div>
+											{/* email */}
+											<div className='form-control'>
+												<label className="block text-[14px] text-[rgba(0,0,0,0.5)] mb-[7px]">email*</label>
+												<input type='email' name='email' placeholder='email *' className='bg-white p-4 w-full capitalize rounded-lg border-4' required />
+											</div>
+											
+											{/* message */}
+											<div className='form-control'>
+												<label className="block text-[14px] text-[rgba(0,0,0,0.5)] mb-[7px]">messages*</label>
+												<textarea type='text' name='msg' placeholder='your messeges' className='bg-white p-4 w-full capitalize rounded-lg border-4' required></textarea>
+											</div>
+                                            {/* file */}
+											<div className='form-control'>
+												<label className="block text-[14px] text-[rgba(0,0,0,0.5)] mb-[7px]">attach file</label>
+												<input type='file' name='attachFile' className='my-4' />
+											</div>
+											{/* submit */}
+											<div className='form-control'>
+												<input type='submit' value={'SEND MESSAGE'} className='fillBtn !bg-black w-full text-white p-4 rounded-lg cursor-pointer font-bold'></input>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default page;
--- src/components/uiComponent/header/Header.jsx
+++ src/components/uiComponent/header/Header.jsx
@@ -8,6 +8,7 @@ import { AiOutlineBars } from "react-icons/ai";
 import { IoClose } from "react-icons/io5";
 import { motion } from "framer-motion"
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const Header = () => {
     const [open, setOpen] = useState(false)
@@ -33,87 +34,96 @@ const Header = () => {
         <div>
             {
                 pathName === '/' ?
-                <header className={header ? ' fixed  w-full mx-auto  shadow-xl px-8 py-6 bg-white z-50' : 'px-8 md:py-8 py-6 bg-transparent fixed  w-full mx-auto z-10'}>
-                <div className="inner-content flex justify-between gap-4">
-                    {/* brand */}
-                    <div className="brand w-32">
-                        <Image src={brand} alt='logo-creatiqucode' />
-                    </div>
-                    {/* for large device */}
-                    <motion.div
-
-                        className="  gap-16 py-1 lg:flex hidden">
-                        <Menu style={' flex gap-4'} />
-                    </motion.div>
-
-                    {/* for large device */}
-                    <div>
-                        <div className="hidden lg:block">
-                            <FillBtn radius={' rounded-full'} btnTitle={'free consultation'} />
-                        </div>
-                        <div className=" bg-[#FF8C24] text-white text-2xl p-2 rounded lg:hidden cursor-pointer">
-                            <div className="" onClick={() => setOpen(!open)}>
-                                <AiOutlineBars />
+                    <header className={header ? ' fixed  w-full mx-auto  shadow-xl px-8 py-6 bg-white z-50' : 'px-8 md:py-8 py-6 bg-transparent fixed  w-full mx-auto z-10'}>
+                        <div className="inner-content flex justify-between gap-4">
+                            {/* brand */}
+                            <div className="brand w-32">
+                                <Image src={brand} alt='logo-creatiqucode' />
                             </div>
-                            {/*  */}
-                            {
-                                open ? <div className="sidebar fixed top-0 left-0 w-full h-screen text-base z-20">
-                                    <div className="menu bg-[#fff] w-60 h-screen p-4">
-                                        <div className="text-[#FF8C24] text-2xl grid justify-items-end gap-6" onClick={() => setOpen(!open)}>
-                                            <IoClose />
-                                        </div>
-                                        <div className="grid gap-20 my-4">
-                                            <Menu style={'grid gap-2 text-black'} />
+                            {/* for large device */}
+                            <motion.div
 
-                                            <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
-                                        </div>
+                                className="  gap-16 py-1 lg:flex hidden">
+                                <Menu style={' flex gap-4'} />
+                            </motion.div>
+
+                            {/* for large device */}
+                            <div>
+                                <div className="hidden lg:block">
+                                    <Link href={'consultaionform'}>
+                                        <FillBtn radius={' rounded-full'} btnTitle={'free consultation'} />
+                                    </Link>
+
+                                </div>
+                                <div className=" bg-[#FF8C24] text-white text-2xl p-2 rounded lg:hidden cursor-pointer">
+                                    <div className="" onClick={() => setOpen(!open)}>
+                                        <AiOutlineBars />
                                     </div>
-                                </div> : null}
-                        </div>
-                    </div>
-                </div>
-            </header>
-                :
-                <header className={header ? ' fixed  w-full mx-auto  shadow-xl px-8 py-6 bg-white z-50' : 'px-8 md:py-8 py-6 bg-transparent fixed  w-full mx-auto z-10'}>
-                <div className="inner-content flex justify-between gap-4">
-                    {/* brand */}
-                    <div className="brand w-32">
-                        <Image src={brand} alt='logo-creatiqucode' />
-                    </div>
-                    {/* for large device */}
-                    <motion.div
+                                    {/*  */}
+                                    {
+                                        open ? <div className="sidebar fixed top-0 left-0 w-full h-screen text-base z-20">
+                                            <div className="menu bg-[#fff] w-60 h-screen p-4">
+                                                <div className="text-[#FF8C24] text-2xl grid justify-items-end gap-6" onClick={() => setOpen(!open)}>
+                                                    <IoClose />
+                                                </div>
+                                                <div className="grid gap-20 my-4">
+                                                    <Menu style={'grid gap-2 text-black'} />
 
-                        className="  gap-16 py-1 lg:flex hidden">
-                        <Menu style={' flex gap-4'} />
-                    </motion.div>
-
-                    {/* for large device */}
-                    <div>
-                        <div className="hidden lg:block">
-                            <FillBtn radius={' rounded-lg !bg-black'} btnTitle={'free consultation'} />
-                        </div>
-                        <div className=" bg-[#FF8C24] text-white text-2xl p-2 rounded lg:hidden cursor-pointer">
-                            <div className="" onClick={() => setOpen(!open)}>
-                                <AiOutlineBars />
+                                                    <Link href={'consultaionform'}>
+                                                        <FillBtn radius={' rounded-full'} btnTitle={'free consultation'} />
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div> : null}
+                                </div>
                             </div>
-                            {/*  */}
-                            {
-                                open ? <div className="sidebar fixed top-0 left-0 w-full h-screen text-base z-20">
-                                    <div className="menu bg-[#fff] w-60 h-screen p-4">
-                                        <div className="text-[#FF8C24] text-2xl grid justify-items-end gap-6" onClick={() => setOpen(!open)}>
-                                            <IoClose />
-                                        </div>
-                                        <div className="grid gap-20 my-4">
-                                            <Menu style={'grid gap-2 text-black'} />
-
-                                            <FillBtn radius={' rounded-sm'} btnTitle={'free consultation'} />
-                                        </div>
-                                    </div>
-                                </div> : null}
                         </div>
-                    </div>
-                </div>
-            </header>
+                    </header>
+                    :
+                    <header className={header ? ' fixed  w-full mx-auto  shadow-xl px-8 py-6 bg-white z-50' : 'px-8 md:py-8 py-6 bg-transparent fixed  w-full mx-auto z-10'}>
+                        <div className="inner-content flex justify-between gap-4">
+                            {/* brand */}
+                            <div className="brand w-32">
+                                <Image src={brand} alt='logo-creatiqucode' />
+                            </div>
+                            {/* for large device */}
+                            <motion.div
+
+                                className="  gap-16 py-1 lg:flex hidden">
+                                <Menu style={' flex gap-4'} />
+                            </motion.div>
+
+                            {/* for large device */}
+                            <div>
+                                <div className="hidden lg:block">
+                                <Link href={'consultaionform'}>
+                                                        <FillBtn radius={' rounded-full'} btnTitle={'free consultation'} />
+                                                    </Link>
+                                </div>
+                                <div className=" bg-[#FF8C24] text-white text-2xl p-2 rounded lg:hidden cursor-pointer">
+                                    <div className="" onClick={() => setOpen(!open)}>
+                                        <AiOutlineBars />
+                                    </div>
+                                    {/*  */}
+                                    {
+                                        open ? <div className="sidebar fixed top-0 left-0 w-full h-screen text-base z-20">
+                                            <div className="menu bg-[#fff] w-60 h-screen p-4">
+                                                <div className="text-[#FF8C24] text-2xl grid justify-items-end gap-6" onClick={() => setOpen(!open)}>
+                                                    <IoClose />
+                                                </div>
+                                                <div className="grid gap-20 my-4">
+                                                    <Menu style={'grid gap-2 text-black'} />
+
+                                                    <Link href={'consultaionform'}>
+                                                        <FillBtn radius={' rounded-full'} btnTitle={'free consultation'} />
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div> : null}
+                                </div>
+                            </div>
+                        </div>
+                    </header>
 
             }
         </div>
